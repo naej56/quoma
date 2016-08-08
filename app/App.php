fdf07dc6bd1a37e9247e06592e176a56303eaa25
naej56 <naej56@hotmail.fr>
2016-08-08 20:18:15 +0200

    mise en place de la connection à la bdd

--- app/App.php
+++ app/App.php
@@ -1,4 +1,7 @@
 <?php 
+use app\database\MysqlDatabase;
+
+
 class App{
 
 	public static function load(){
@@ -6,6 +9,24 @@ class App{
 		require ROOT . '/app/Autoloader.php';
 		app\Autoloader::register();
 	}
+
+	public static function getInstance()
+	{
+		if(is_null(self::$_instance)){
+			self::$_instance = new App();
+		}
+		return self::$_instance;
+	}
+
+	public function getDb()
+	{
+		$param = Config::getInstance(ROOT . '/config/param.php');
+		if(is_null($this->db_instance)){
+			$this->db_instance = new MysqlDatabase($param->get('db_name'), $param->get('db_user'), $param->get('dn_pass'), $param->get('db_host'));	
+		}
+		return $this->db_instance;
+	}
+
 }
 
 
--- app/database/MysqlDatabase.php
+++ app/database/MysqlDatabase.php
@@ -4,7 +4,54 @@ use app\database\Database;
 
 
 class MysqlDatabase extends Database{
-	
+	private $db_name;
+	private $db_user;
+	private $db_pwd;
+	private $db_host;
+	private $db;
+
+	public function __contruct($db_name = 'quoma', $db_user = 'quoma', $db_pwd = 'quoma', $db_host = 'localhost'){
+		$this->db_name = $db_name;
+		$this->db_user = $db_user;
+		$this->db_pwd = $db_pwd;
+		$this->db_host = $db_host;
+	}
+
+	private function getDb(){
+		if ($this->db === null){
+			$db = new PDO('mysql:dbname=offinote;host=localhost', 'offinote', 'offinote');
+			$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			$this->db = $db;	
+		}
+		return $this->db;
+	}
+
+	public function query($rqte, $class_name = null, $one = false){
+		$requete = $this->getDb()->query($rqte);
+		if($class_name === null){
+			$requete->setFetchMode(PDO::FETCH_OBJ);
+		} else {
+			$requete->setFetchMode(PDO::FETCH_CLASS, $class_name);
+		}
+		if($one){
+			$data = $requete->fetch();
+		} else {
+			$data = $requete->fetchAll();
+		}
+		return $data;
+	}
+
+	public function prepare($rqte, $attribues, $class_name, $one = false){
+		$requete = $this->getDb()->prepare($rqte);
+		$requete->execute($attribues);
+		$requete->setFetchMode(PDO::FETCH_CLASS, $class_name);
+		if($one){
+			$data = $requete->fetch();	
+		} else {
+			$data = $requete->fetchAll();	
+		}
+		return $data;
+	}
 }
 
  ?>
